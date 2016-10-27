a7cd0c1a180281d257ef712eee02ecd610036bbf
weiting123 <wt572051341@163.com>
2016-10-27 18:42:09 +0800

    common:rspec

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,17 @@ Bundler.require(*Rails.groups)
 
 module WeitingSisningCom
   class Application < Rails::Application
+    # Setting for Rspec
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_spece: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
