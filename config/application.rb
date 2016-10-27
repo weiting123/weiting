157200521e1d48c419470d02c0c48450dd4eb7b1
weiting123 <wt572051341@163.com>
2016-10-27 18:42:15 +0800

    web:app

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ Bundler.require(*Rails.groups)
 
 module WeitingSisningCom
   class Application < Rails::Application
+    Slim::Engine.options[:pretty] = true
+    config.generators.helper = false
+    config.generators.assets = false
     # Setting for Rspec
     config.generators do |g|
       g.test_framework :rspec,
